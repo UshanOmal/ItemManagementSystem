19ed7b37c1ac687548dc07ee3000406dab4d6bfd
ushansilva <ushans@axionsolutions.biz>
2023-05-16 12:05:15 +0530

    Project added

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ItemManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ItemManagementSystem.Context
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options) 
+        { 
+        
+        
+        }
+
+        public DbSet<Item> Items { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Data/Migrations/20230516052632_InitialMigration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ItemManagementSystem.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TaskName = table.Column<string>(type: "text", nullable: false),
+                    TaskDescription = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    Attachement = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
